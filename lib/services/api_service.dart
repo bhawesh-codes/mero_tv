94a2e955e9af592c6ee5903f014ab6421e8b651d
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-03 20:32:00 +0545

    feat: get streams data from api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:mero_tv/models/channel_model.dart';
 import 'package:mero_tv/models/logo_model.dart';
+import 'package:mero_tv/models/stream_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'api_service.g.dart';
@@ -9,9 +10,13 @@ part 'api_service.g.dart';
 abstract class ApiService {
   factory ApiService(Dio dio, {String? baseUrl}) = _ApiService;
 
-@GET('channels.json')
-Future<List<ChannelModel>> getChannels();
+  @GET('channels.json')
+  Future<List<ChannelModel>> getChannels();
 
-@GET('logos.json')
-Future<List<LogoModel>> getLogos();
+  @GET('logos.json')
+  Future<List<LogoModel>> getLogos();
+
+  @GET('streams.json')
+  Future<List<StreamModel>> getStreams();
+
 }
--- lib/services/api_service.g.dart
+++ lib/services/api_service.g.dart
@@ -77,6 +77,35 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<List<StreamModel>> getStreams() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<List<LogoModel>>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            'streams.json',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<List<dynamic>>(_options);
+    late List<StreamModel> _value;
+    try {
+      _value = _result.data!
+          .map((dynamic i) => StreamModel.fromJson(i as Map<String, dynamic>))
+          .toList();
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
