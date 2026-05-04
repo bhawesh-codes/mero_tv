48c932071cfb3d7ccd0734a93e3c1fe844a939f1
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-04 16:40:54 +0545

    feat: add required colors in app_color

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-const Color kcPrimaryColor = Color(0xFF9600FF);
-const Color kcPrimaryColorDark = Color(0xFF300151);
-const Color kcDarkGreyColor = Color(0xFF1A1B1E);
-const Color kcMediumGrey = Color(0xFF474A54);
-const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
-const Color kcVeryLightGrey = Color(0xFFE3E3E3);
-const Color kcBackgroundColor = kcDarkGreyColor;
+const Color kcBackgroundColor = Color(0xFF000000);
+// const Color kcPrimaryColorDark = Color(0xFF300151);
+const Color kcSurfaceColor = Color(0xFF1E1E1E);
+const Color kcPrimaryColor = Color(0xffE50914);
+const Color kcSoftAccentColor = Color(0xffFF3B3B);
+const Color kcPrimaryTextColor = Color(0xffFFFFFF);
+const Color kcSecondaryTextColor = Color(0xffB3B3B3);
+const Color kcDisabledTextColor = Color(0xff808080);
