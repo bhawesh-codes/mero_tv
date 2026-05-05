b1b42d449a5589ce7acfbfe2a1ec9bdc6ab18fe3
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-05 16:27:32 +0545

    feat: add functional error handling in repository

--- lib/repository/channel_repository.dart
+++ lib/repository/channel_repository.dart
@@ -1,38 +1,32 @@
+import 'package:fpdart/fpdart.dart';
 import 'package:injectable/injectable.dart';
-import 'package:mero_tv/models/channel_model.dart';
+import 'package:mero_tv/core/failures/failures.dart';
+import 'package:mero_tv/core/failures/failures_mapper.dart';
 import 'package:mero_tv/models/logo_model.dart';
 import 'package:mero_tv/models/stream_model.dart';
-import 'package:mero_tv/repository/channel_repository_base.dart';
 import 'package:mero_tv/services/api_service.dart';
 
 @lazySingleton
-class ChannelRepository implements ChannelRepositoryBase{
+class ChannelRepository {
   final ApiService _apiService;
   ChannelRepository(this._apiService);
-  @override
-  Future<List<ChannelModel>> getChannels() async {
-    try{ return await _apiService.getChannels();
-    }catch(e){
-      throw Exception(e.toString());
-    }
-    
-  }
 
-  @override
-  Future<List<LogoModel>> getLogos()async {
-    try{
-      return await _apiService.getLogos();
-    }catch(e){
-      throw Exception(e.toString());
-    }
-  }
-
-  @override
-  Future<List<StreamModel>> getStreams() async {
+  Future<Either<Failure, List<StreamModel>>> getStreams() async {
     try {
-      return await _apiService.getStreams();
+      final result = await _apiService.getStreams();
+      if (result.isEmpty) return left(const NoDataFailure('No streams found.'));
+      return right(result);
     } catch (e) {
-      throw Exception(e.toString());
+      return left(FailureMapper.fromException(e));
+    }
+  }
+
+  Future<Either<Failure, List<LogoModel>>> getLogos() async {
+    try {
+      final result = await _apiService.getLogos();
+      return right(result);
+    } catch (e) {
+      return left(FailureMapper.fromException(e));
     }
   }
 }
