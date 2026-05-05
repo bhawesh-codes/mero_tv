15c8062003ad836aeef08d2f7b0be3d9e95d8310
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-05 13:10:10 +0545

    feat: create a main view to handle view according to current index

---
+++ lib/ui/views/main_view/main_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:mero_tv/ui/common/app_colors.dart';
+import 'package:mero_tv/ui/views/favorites/favorites_view.dart';
+import 'package:mero_tv/ui/views/home/home_view.dart';
+import 'package:mero_tv/ui/views/main_view/main_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+
+class MainView extends StackedView<MainViewModel> {
+  const MainView({super.key});
+
+  @override
+  Widget builder(BuildContext context, MainViewModel viewModel, Widget? child) {
+    return Scaffold(
+      body: viewModel.currentIndex == 0
+          ? const HomeView()
+          : const FavoritesView(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: viewModel.currentIndex,
+        onTap: viewModel.setIndex,
+        backgroundColor: const Color.fromARGB(255, 11, 11, 11),
+        selectedItemColor: kcPrimaryColor,
+        unselectedItemColor: kcDisabledTextColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.live_tv),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  MainViewModel viewModelBuilder(BuildContext context) => MainViewModel();
+}
