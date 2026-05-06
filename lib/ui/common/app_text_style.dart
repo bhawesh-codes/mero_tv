a0354d0e2b84bb9fca1d26d6778c0837654d8ed9
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-06 14:27:24 +0545

    feat: create text styles

---
+++ lib/ui/common/app_text_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+final TextStyle titleLarge =
+    TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700);
+final TextStyle titleSmall =
+    TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w700);
+
+final TextStyle bodyLarge =
+    TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w600);
+
+final TextStyle bodyMedium =
+    TextStyle(fontSize: 18.sp, fontWeight: FontWeight.normal);
+
+final TextStyle bodySmall =
+    TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400);
+
+final TextStyle titleMedium =
+    TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600);
