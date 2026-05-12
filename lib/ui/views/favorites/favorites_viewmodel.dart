a603607b77046caa0ade6c2c53f042e8c68123c5
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-12 09:58:49 +0545

    refactor: replace StreamModel with ChannelModel in FavoritesService and FavoritesViewModel

--- lib/ui/views/favorites/favorites_viewmodel.dart
+++ lib/ui/views/favorites/favorites_viewmodel.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'package:mero_tv/app/app.locator.dart';
-import 'package:mero_tv/models/stream_model.dart';
+import 'package:mero_tv/models/channel_model.dart';
 import 'package:mero_tv/ui/views/favorites/services/favorites_service.dart';
 import 'package:stacked/stacked.dart';
 
@@ -11,10 +11,10 @@ class FavoritesViewModel extends BaseViewModel {
   final FavoritesService _favoritesService = locator<FavoritesService>();
 
   // expose listenable to view
-  ValueListenable<Box<StreamModel>> get favoritesListenable =>
+  ValueListenable<Box<ChannelModel>> get favoritesListenable =>
       _favoritesService.listenable;
 
-  Future<void> toggleFavorite(StreamModel channel) async {
+  Future<void> toggleFavorite(ChannelModel channel) async {
     await _favoritesService.toggleFavorite(channel);
   }
 }
--- lib/ui/views/favorites/services/favorites_service.dart
+++ lib/ui/views/favorites/services/favorites_service.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:injectable/injectable.dart';
-import 'package:mero_tv/models/stream_model.dart';
+import 'package:mero_tv/models/channel_model.dart';
 
 @LazySingleton()
 class FavoritesService {
-  Box<StreamModel> get _box => Hive.box<StreamModel>('favorites');
+  Box<ChannelModel> get _box => Hive.box<ChannelModel>('favorites');
 
   // expose listenable for reactive UI
-  ValueListenable<Box<StreamModel>> get listenable => _box.listenable();
+  ValueListenable<Box<ChannelModel>> get listenable => _box.listenable();
 
-  List<StreamModel> getFavorites() => _box.values.toList();
+  List<ChannelModel> getFavorites() => _box.values.toList();
 
-  bool isFavorite(StreamModel channel) => _box.containsKey(channel.channel);
+  bool isFavorite(ChannelModel channel) => _box.containsKey(channel.id);
 
-  Future<void> toggleFavorite(StreamModel channel) async {
+  Future<void> toggleFavorite(ChannelModel channel) async {
     if (isFavorite(channel)) {
-      await _box.delete(channel.channel);
+      await _box.delete(channel.id);
     } else {
-      await _box.put(channel.channel, channel);
+      await _box.put(channel.id, channel);
     }
   }
 }
