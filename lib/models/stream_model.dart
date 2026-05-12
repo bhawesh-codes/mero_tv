9f5e4f4e97965bce298c4c00db50569c2b3e0c76
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-05-12 09:57:57 +0545

    refactor: remove Hive type adapter from StreamModel and clean up generated code

--- lib/models/stream_model.dart
+++ lib/models/stream_model.dart
@@ -1,37 +1,27 @@
-import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'stream_model.g.dart';
 
 @JsonSerializable()
-@HiveType(typeId: 0)
 class StreamModel {
-  @HiveField(0)
+  
   String? channel;
 
-  @HiveField(1)
   String? feed;
 
-  @HiveField(2)
   String? title;
 
-  @HiveField(3)
   String? url;
 
-  @HiveField(4)
   String? quality;
 
-  @HiveField(5)
   String? label;
 
-  @HiveField(6)
   @JsonKey(name: 'user_agent')
   String? userAgent;
 
-  @HiveField(7)
   String? referrer;
 
-  @HiveField(8)
   String? logoUrl;
 
   StreamModel({
--- lib/models/stream_model.g.dart
+++ lib/models/stream_model.g.dart
@@ -3,68 +3,6 @@
 part of 'stream_model.dart';
 
 // **************************************************************************
-// TypeAdapterGenerator
-// **************************************************************************
-
-class StreamModelAdapter extends TypeAdapter<StreamModel> {
-  @override
-  final int typeId = 0;
-
-  @override
-  StreamModel read(BinaryReader reader) {
-    final numOfFields = reader.readByte();
-    final fields = <int, dynamic>{
-      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
-    };
-    return StreamModel(
-      channel: fields[0] as String?,
-      feed: fields[1] as String?,
-      title: fields[2] as String?,
-      url: fields[3] as String?,
-      quality: fields[4] as String?,
-      label: fields[5] as String?,
-      userAgent: fields[6] as String?,
-      referrer: fields[7] as String?,
-      logoUrl: fields[8] as String?,
-    );
-  }
-
-  @override
-  void write(BinaryWriter writer, StreamModel obj) {
-    writer
-      ..writeByte(9)
-      ..writeByte(0)
-      ..write(obj.channel)
-      ..writeByte(1)
-      ..write(obj.feed)
-      ..writeByte(2)
-      ..write(obj.title)
-      ..writeByte(3)
-      ..write(obj.url)
-      ..writeByte(4)
-      ..write(obj.quality)
-      ..writeByte(5)
-      ..write(obj.label)
-      ..writeByte(6)
-      ..write(obj.userAgent)
-      ..writeByte(7)
-      ..write(obj.referrer)
-      ..writeByte(8)
-      ..write(obj.logoUrl);
-  }
-
-  @override
-  int get hashCode => typeId.hashCode;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is StreamModelAdapter &&
-          runtimeType == other.runtimeType &&
-          typeId == other.typeId;
-}
-
-// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
