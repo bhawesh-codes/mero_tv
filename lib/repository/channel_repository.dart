4a1568f035b8ecbfce9aa2fc1bc61d4519126b3d
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-06-10 16:07:02 +0545

    fix: correct getChannels method error handling and ensure proper formatting

--- lib/repository/channel_repository.dart
+++ lib/repository/channel_repository.dart
@@ -1,8 +1,10 @@
+// lib/repository/channel_repository.dart
 import 'package:fpdart/fpdart.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mero_tv/core/failures/failures.dart';
 import 'package:mero_tv/core/failures/failures_mapper.dart';
 import 'package:mero_tv/models/channel_model.dart';
+import 'package:mero_tv/models/country_model.dart';
 import 'package:mero_tv/models/logo_model.dart';
 import 'package:mero_tv/models/stream_model.dart';
 import 'package:mero_tv/repository/channel_repository_base.dart';
@@ -11,6 +13,7 @@ import 'package:mero_tv/services/api_service.dart';
 @lazySingleton
 class ChannelRepository implements ChannelRepositoryBase {
   final ApiService _apiService;
+
   ChannelRepository(this._apiService);
 
   @override
@@ -35,12 +38,24 @@ class ChannelRepository implements ChannelRepositoryBase {
   }
 
   @override
-  Future<Either<Failure, List<ChannelModel>>> getChannels() async{
+  Future<Either<Failure, List<ChannelModel>>> getChannels() async {
     try {
       final result = await _apiService.getChannels();
       return right(result);
+    } catch (e) {
+      return left(FailureMapper.fromException(e));
+    }
+  }
 
-    }catch (e){
+  @override
+  Future<Either<Failure, List<CountryModel>>> getCountries() async {
+    try {
+      final result = await _apiService.getCountries();
+      if (result.isEmpty) {
+        return left(const NoDataFailure('No countries found.'));
+      }
+      return right(result);
+    } catch (e) {
       return left(FailureMapper.fromException(e));
     }
   }
