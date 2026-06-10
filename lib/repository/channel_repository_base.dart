e2707822b9a54e2668e7acc6b16a80de3d4851f7
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-06-10 16:06:50 +0545

    feat: add getCountries method to ChannelRepositoryBase

--- lib/repository/channel_repository_base.dart
+++ lib/repository/channel_repository_base.dart
@@ -1,13 +1,14 @@
-
+// lib/repository/channel_repository_base.dart
 import 'package:fpdart/fpdart.dart';
 import 'package:mero_tv/core/failures/failures.dart';
 import 'package:mero_tv/models/channel_model.dart';
+import 'package:mero_tv/models/country_model.dart';
 import 'package:mero_tv/models/logo_model.dart';
 import 'package:mero_tv/models/stream_model.dart';
 
 abstract class ChannelRepositoryBase {
-
   Future<Either<Failure, List<StreamModel>>> getStreams();
   Future<Either<Failure, List<LogoModel>>> getLogos();
   Future<Either<Failure, List<ChannelModel>>> getChannels();
+  Future<Either<Failure, List<CountryModel>>> getCountries();
 }
