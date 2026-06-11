476ff7e6a3ab0b389c38e1f45f11aa01faa2103a
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-06-11 10:10:20 +0545

    feat: implement Picture-in-Picture mode support in MainActivity

--- android/app/src/main/kotlin/com/bhawesh/mero_tv/MainActivity.kt
+++ android/app/src/main/kotlin/com/bhawesh/mero_tv/MainActivity.kt
@@ -1,5 +1,56 @@
 package com.bhawesh.mero_tv
 
+import android.app.PictureInPictureParams
+import android.content.res.Configuration
+import android.os.Build
+import android.util.Rational
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val PIP_CHANNEL = "mero_tv/pip"
+    private var pipMethodChannel: MethodChannel? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        pipMethodChannel = MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            PIP_CHANNEL
+        )
+
+        pipMethodChannel!!.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "enterPip" -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        val params = PictureInPictureParams.Builder()
+                            .setAspectRatio(Rational(16, 9))
+                            .build()
+                        enterPictureInPictureMode(params)
+                        result.success(true)
+                    } else {
+                        result.error(
+                            "UNSUPPORTED",
+                            "PiP not supported on this Android version",
+                            null
+                        )
+                    }
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+
+    // Notify Flutter when PiP mode changes so it can hide/show UI
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        pipMethodChannel?.invokeMethod(
+            "onPipChanged",
+            mapOf("isInPip" to isInPictureInPictureMode)
+        )
+    }
+}
