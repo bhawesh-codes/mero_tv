aca0e258c8ea7357f28d20ada8d1144f4a217035
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-06-12 10:38:21 +0545

    feat: update FavoritesBody to set channels for swipe gesture navigation

--- lib/ui/views/favorites/widgets/favorites_body.dart
+++ lib/ui/views/favorites/widgets/favorites_body.dart
@@ -1,6 +1,8 @@
 // widgets/favorites_body.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mero_tv/app/app.locator.dart';
+import 'package:mero_tv/services/channel_player_service.dart';
 import 'package:mero_tv/ui/views/favorites/widgets/empty_favorites_widgets.dart';
 import 'package:stacked/stacked.dart';
 import '../../video_player/video_player_view.dart';
@@ -8,21 +10,19 @@ import '../favorites_viewmodel.dart';
 import 'favorite_card.dart';
 
 class FavoritesBody extends StackedView<FavoritesViewModel> {
-
   const FavoritesBody({Key? key}) : super(key: key);
 
   @override
-  Widget builder(BuildContext context, FavoritesViewModel viewModel, Widget? child) {
+  Widget builder(
+      BuildContext context, FavoritesViewModel viewModel, Widget? child) {
     return SafeArea(
       child: ValueListenableBuilder(
         valueListenable: viewModel.favoritesListenable,
         builder: (context, box, _) {
           final favorites = box.values.toList();
-
           if (favorites.isEmpty) {
             return const EmptyFavoritesWidget();
           }
-
           return Padding(
             padding: EdgeInsets.all(12.r),
             child: ListView.builder(
@@ -32,7 +32,12 @@ class FavoritesBody extends StackedView<FavoritesViewModel> {
                 return FavoriteCard(
                   channel: channel,
                   onTap: () {
-                    if (channel.streamUrl!= null) {
+                    if (channel.streamUrl != null) {
+                      // Set the favorites list + tapped index so the
+                      // player's swipe gesture cycles through favorites
+                      locator<ChannelPlayerService>()
+                          .setChannels(favorites, index);
+
                       Navigator.push(
                         context,
                         MaterialPageRoute(
@@ -53,9 +58,8 @@ class FavoritesBody extends StackedView<FavoritesViewModel> {
       ),
     );
   }
+
   @override
   FavoritesViewModel viewModelBuilder(BuildContext context) =>
       FavoritesViewModel();
-      
-        
 }
